a59dfed09bb8ef9877fd7157b0e25130a545b963
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-04-09 13:05:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let result ="";
+	if(shortHex.length ===4){
+		let r,g,b;
+		r = shortHex[1];
+		g = shortHex[2];
+		b = shortHex[3];
+		let hexcode = "#"+r+r+g+g+b+b;
+		result += hexcode;
+	}
+	if(shortHex.length===3){
+		let r,g,b;
+		r = shortHex[0];
+		g = shortHex[1];
+		b = shortHex[2];
+		let hexcode = "#"+r+r+g+g+b+b;
+		result += hexcode;
+	}
+
+	return result;
 };
 
 // Do not change the code below.
